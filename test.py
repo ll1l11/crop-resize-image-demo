0080b8e12da3893f1f9aeb76947fda466fc8a091
codeif <me@codeif.com>
2016-09-07 19:50:29 +0800

    重构, 加注

--- test.py
+++ test.py
@@ -3,14 +3,9 @@ from PIL import Image
 
 
 def get_target_size(img_size, size, exact_size=False):
-    """get target image size"""
     assert img_size[0] and img_size[1]
     assert size[0] or size[1]
 
-    # calc by formula:
-    # x / y == m / n
-    # =>
-    # x * n == y * m
     size = list(size)
     if not size[0]:
         size[0] = size[1] * img_size[0] // img_size[1]
@@ -23,15 +18,20 @@ def get_target_size(img_size, size, exact_size=False):
         return tuple(size)
 
 
-def crop_by_scale(img, width, height):
-    size = img.size
-    size1 = (size[0], size[0] * height // width)
-    size2 = (size[1] * width // height, size[1])
-    new_size = min(size1, size2)
-    print('按照比例剪裁', new_size)
+def crop_by_aspect_ratio(image, aspect_ratio):
+    """crop image by scale without aspect ratio distortion
 
-    if new_size == img.size:
-        return img
+    :param image: a PIL image object
+    :param aspect_ratio: aspect ratio,  as a 2-tuple: (width, height).
+    :returns: An :py:class:`~PIL.Image.Image` object.
+    """
+    size = image.size
+    size1 = (size[0], size[0] * aspect_ratio[1] // aspect_ratio[0])
+    size2 = (size[1] * aspect_ratio[0] // aspect_ratio[1], size[1])
+    new_size = min(size1, size2)
+
+    if new_size == image.size:
+        return image
 
     # calc left, upper, right, lower
     left = (size[0] - new_size[0]) // 2
@@ -39,34 +39,25 @@ def crop_by_scale(img, width, height):
     upper = (size[1] - new_size[1]) // 2
     lower = upper + new_size[1]
 
-    return img.crop((left, upper, right,  lower))
+    return image.crop((left, upper, right, lower))
 
 
-def crop_resize(img, size, exact_size=False):
-    # resize
-    target_size = get_target_size(img.size, size, exact_size)
-    print('生成的图片大小: ', target_size)
-    img2 = crop_by_scale(img, target_size[0], target_size[1])
+def crop_resize(image, size, exact_size=False):
+    """
+
+    :param image: a PIL image object
+    :param size: a 2-tuple of (width,height);  at least one must be specified
+    :param exact_size: whether to scale up for smaller images.
+        Defaults to ``False``.
+    :return: An :py:class:`~PIL.Image.Image` object.
+    """
+    target_size = get_target_size(image.size, size, exact_size)
+    img2 = crop_by_aspect_ratio(image, target_size[0], target_size[1])
     return img2.resize(target_size, Image.ANTIALIAS)
 
 
-# def resize_crop(img, max_width, max_height):
-#     target_size = calc_target_size(img.size, max_width, max_height)
-#     size1 = (target_size[0], target_size[0] * img.size[1] // img.size[0])
-#     size2 = (target_size[1] * img.size[0] // img.size[1], target_size[1])
-#     print(size1, size2)
-#     img2 = img.resize(max(size1, size2))
-#     print('缩放后图片大小', img2.size)
-#     return crop_by_scale(img2, target_size[0], target_size[1])
-
-
 def test():
-    img = Image.open('chang.png')
-    # img = Image.open('/Users/xyz/Downloads/3.jpg')
-    print('原图片大小:', img.size)
-
-    # width = 200
-    # height = 200
+    img = Image.open('img.png')
     size = (255, 255)
     exact_size = False
     img3 = crop_resize(img, size, exact_size)
@@ -75,3 +66,4 @@ def test():
 
 if __name__ == '__main__':
     test()
+
